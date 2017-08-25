d137850a2d70f24e50d38cc9f97c5e7c88377f4a
Aditya Joardar <30207466+joardar-aditya@users.noreply.github.com>
2017-08-25 20:06:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,9 @@ app.get('/article-two',function(req, res){
 app.get('/article-three',function(req, res){
     res.send('Article three will be dislayed here!');
 });
-
+app.get('/ui/main.js', function(req, res) {
+    res.sendFile(path.join(_dirname, 'ui' , 'main.js' ));
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
